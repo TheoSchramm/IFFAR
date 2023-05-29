af3c46bf627d1d1603d3e8b6b6fe146983d95f44
Théo Pata Schramm <105531805+TheoSchramm@users.noreply.github.com>
2023-05-29 18:43:29 -0300

    Add files via upload

---
+++ m.py
@@ -0,0 +1,56 @@
+import random
+import matplotlib.pyplot as plt
+import time
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    metade = len(arr) // 2
+    
+    # Divide o vetor fazendo uso dos operadores de "slice"
+    esquerda = arr[:metade]
+    direita = arr[metade:]
+    
+    # Recursivamente divide os vetores laterais
+    esquerda = merge_sort(esquerda)
+    direita = merge_sort(direita)
+    
+    # Junta novamente as duas metades laterais
+    return merge(esquerda, direita)
+
+
+def merge(esquerda, direita):
+    resultado = []
+    # É criado duas variaveis que irão percorrer os vetor laterais
+    i = j = 0
+
+    # Compara os elementos da esquerda e da direta e anexa o menor elemento ao resultado
+    while i < len(esquerda) and j < len(direita):
+        if esquerda[i] <= direita[j]:
+            resultado.append(esquerda[i])
+            i += 1
+        else:
+            resultado.append(direita[j])
+            j += 1
+    
+    # Anexa os elementos restantes
+    resultado.extend(esquerda[i:])
+    resultado.extend(direita[j:])
+
+    return resultado
+
+def main():
+    vetor = random.sample(population=range(1,11), k=10)
+    
+    fig, ax = plt.subplots()
+    ax.bar(range(len(merge_sort(vetor))), merge_sort(vetor))
+    
+    plt.ylabel("Número")
+    plt.xlabel("Posição")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
