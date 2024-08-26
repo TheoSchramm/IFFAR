06466e0036f0a64a72a8f81eac45c5c7d244678b
Théo Pata Schramm <theopsschramm@gmail.com>
2024-08-26 14:32:38 -0300

    sgtm

--- ProgWeb/src/app/app.config.ts
+++ ProgWeb/src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
+import { env } from '../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(env.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    { provide: FIREBASE_OPTIONS, useValue: env.firebaseConfig },
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
 };
